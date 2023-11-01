3ac55491226fdeb6b395773bdc7ab96cf15cbdbb
minhnghia2k3 <BrozennVN@gmail.com>
2023-11-01 22:57:37 +0700

    feat: Add Homepage

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,6 +1,10 @@
 import { ClerkProvider } from "@clerk/nextjs"
 import { Inter } from "next/font/google"
 import "../globals.css"
+import Topbar from "@/components/shared/Topbar"
+import LeftSidebar from "@/components/shared/LeftSidebar"
+import RightSidebar from "@/components/shared/RightSidebar"
+import Bottombar from "@/components/shared/Bottombar"
 export const metadata = {
   title: 'Threads',
   description: 'A Next.js 14 Meta Threads Application!'
@@ -12,8 +16,20 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={`${inter.className} bg-dark-1`}>
-          {children}
+        <body className={`${inter.className}`}>
+          <Topbar />
+
+          <main className="flex flex-row">
+            <LeftSidebar />
+            <section className="main-container">
+              <div className="w-full max-w-4xl">
+                {children}
+              </div>
+            </section>
+            <RightSidebar />
+          </main>
+
+          <Bottombar />
         </body>
       </html>
     </ClerkProvider>
